31ec52e91f40971b528902f904b36e90fdbb61db
Damian Barahona <102879758+Damian2044@users.noreply.github.com>
2025-08-28 09:59:36 -0500

    Update axios.jsx

--- frontend/app-frontend/src/lib/axios.jsx
+++ frontend/app-frontend/src/lib/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api=axios.create({
-    baseURL: "http://localhost:8000"//"http://192.168.100.20:8000"//import.meta.env.VITE_API_URL 
+    baseURL: import.meta.env.VITE_API_URL//"http://localhost:8000" 
 });
 
 export default api;
